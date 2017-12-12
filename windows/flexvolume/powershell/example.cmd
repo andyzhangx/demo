b75a23519b1497aca90a8431579c0add34bcf970
Andy Zhang <xiazhang@microsoft.com>
2017-12-12 16:53:05 +0800

    Update example.cmd

--- windows/flexvolume/powershell/example.cmd
+++ windows/flexvolume/powershell/example.cmd
@@ -1,4 +1,4 @@
 @echo off
-rem need to find a way to pass json parameters which contains "
-rem powershell C:\volumeplugins\test~example2.cmd\example.ps1 %*
-powershell C:\volumeplugins\test~example2.cmd\example.ps1 %~1 %~2 "test"
+rem need to find a better way to pass json parameters which contains "
+rem powershell C:\volumeplugins\test~example.cmd\example.ps1 %*
+powershell C:\volumeplugins\test~example.cmd\example.ps1 %~1 %~2 '%~3' '%~4'
