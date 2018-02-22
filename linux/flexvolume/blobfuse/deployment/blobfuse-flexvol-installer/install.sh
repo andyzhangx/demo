77ffaf1689ba02dc0c56aab7418d5d2371ef20bc
andyzhangx <xiazhang@microsoft.com>
2018-02-22 05:04:28 +0000

    fix blobfuse k8s deployment issue

--- linux/flexvolume/blobfuse/deployment/blobfuse-flexvol-installer/install.sh
+++ linux/flexvolume/blobfuse/deployment/blobfuse-flexvol-installer/install.sh
@@ -12,9 +12,14 @@ blobfuse_vol_dir="${target_dir}/azure~blobfuse"
 blobfuse_bin_dir="${blobfuse_vol_dir}/bin"
 mkdir -p ${blobfuse_bin_dir}
 
-cp /usr/bin/blobfuse ${blobfuse_bin_dir}/		#binary
-cp /root/blobfuse ${blobfuse_vol_dir}/blobfuse 	#script
-cp /usr/bin/jq ${blobfuse_vol_dir}/jq
+#apt-get update && apt-get install apt-transport-https jq wget curl -y
+#wget -O /tmp/packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+#dpkg -i /tmp/packages-microsoft-prod.deb
+#apt-get update && apt-get install blobfuse fuse -y
+
+#cp /usr/bin/blobfuse ${blobfuse_bin_dir}/		#binary
+wget -O ${blobfuse_vol_dir}/blobfuse https://raw.githubusercontent.com/andyzhangx/Demo/master/linux/flexvolume/blobfuse/blobfuse
+#cp /usr/bin/jq ${blobfuse_vol_dir}/jq
 
 #https://github.com/kubernetes/kubernetes/issues/17182
 # if we are running on kubernetes cluster as a daemon set we should
