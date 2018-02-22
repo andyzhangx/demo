d5fa08311eeb1fc025136bb05d4f783953ca5990
andyzhangx <xiazhang@microsoft.com>
2018-02-22 08:56:38 +0000

    update install.sh script for blobfuse flex vol

--- linux/flexvolume/blobfuse/deployment/blobfuse-flexvol-installer/install.sh
+++ linux/flexvolume/blobfuse/deployment/blobfuse-flexvol-installer/install.sh
@@ -9,12 +9,19 @@ if [[ -z "${target_dir}" ]];then
   target_dir="/etc/kubernetes/volumeplugins"
 fi
 
+apt-get update && apt-get install wget -y >>$LOG
+
 blobfuse_vol_dir="${target_dir}/azure~blobfuse"
 blobfuse_bin_dir="${blobfuse_vol_dir}/bin"
 mkdir -p ${blobfuse_bin_dir}
 
 #download blobfuse binary
-version=`kubelet --version | awk -F ' ' '{print $2}' | awk -F '.' '{print $1"."$2}'`
+version="v1.9"
+kubelet_location=`whereis kubelet | awk -F ' ' '{print $2}'`
+if [[ $kubelet_location == *"kubelet" ]]; then
+	version=`kubelet --version | awk -F ' ' '{print $2}' | awk -F '.' '{print $1"."$2}'`
+fi
+
 wget -O ${blobfuse_bin_dir}/blobfuse https://raw.githubusercontent.com/andyzhangx/Demo/master/linux/flexvolume/blobfuse/binary/kubelet/$version/blobfuse >>$LOG	
 chmod a+x ${blobfuse_bin_dir}/blobfuse
 
