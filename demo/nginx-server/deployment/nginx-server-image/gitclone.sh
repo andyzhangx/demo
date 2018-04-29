47ed4c33ca7106c6dce0ab41e6b4922d9c8377df
andyzhangx <xiazhang@microsoft.com>
2018-04-29 14:38:57 +0000

    update nginx-server example

---
+++ demo/nginx-server/deployment/nginx-server-image/gitclone.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+DIR="/mnt/azurefile"
+LOG="/var/log/gitclone.log"
+
+if [ -d "$DIR" ]; then
+	if [ "$(ls -A $DIR)" ]; then
+		echo "$DIR is empty" >> $LOG
+	else
+		git clone https://github.com/andyzhangx/demo.git $DIR 2>&1 >> $LOG
+	fi
+else
+	echo "$DIR does not exist" >> $LOG
+fi
