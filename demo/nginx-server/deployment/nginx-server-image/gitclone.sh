cff5bb8f7e5b079bd2c6185d507670f366476a63
andyzhangx <xiazhang@microsoft.com>
2018-06-20 05:45:16 +0000

    update nginx-server-azurefile.yaml

--- demo/nginx-server/deployment/nginx-server-image/gitclone.sh
+++ demo/nginx-server/deployment/nginx-server-image/gitclone.sh
@@ -6,7 +6,7 @@ if [ -d "$DIR" ]; then
 	if [ "$(ls -A $DIR)" ]; then
 		echo "$DIR is empty" >> $LOG
 	else
-		git clone https://github.com/andyzhangx/kubernetes-drivers.git $DIR 2>&1 >> $LOG
+		git clone https://github.com/Azure/kubernetes-volume-drivers.git $DIR 2>&1 >> $LOG
 	fi
 else
 	echo "$DIR does not exist" >> $LOG
