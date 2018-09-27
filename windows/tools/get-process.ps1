069f1b839f7d7c496f2af6d50cf9a34ffcd9118d
Andy Zhang <xiazhang@microsoft.com>
2018-09-27 16:58:00 +0800

    Update get-process.ps1

--- windows/tools/get-process.ps1
+++ windows/tools/get-process.ps1
@@ -1,7 +1,7 @@
 $LOG="c:\getprocess.txt"
 while ($true)
 {
-	Get-Process  | Sort-Object -Property PrivateMemorySize -Descending  | Select-Object -First 20 | select-object @{l="Private Memory (MB)"; e={$_.PrivateMemorySize/ 1mb}}, CPU, Name >> $LOG
+	Get-Process  | Sort-Object -Property PrivateMemorySize -Descending  | Select-Object -First 10 | select-object @{l="Private Memory (MB)"; e={$_.PrivateMemorySize/ 1mb}}, CPU, Name >> $LOG
 	Get-counter -Counter "\memory\% committed bytes in use" >> $LOG
 	Sleep 60
 }
