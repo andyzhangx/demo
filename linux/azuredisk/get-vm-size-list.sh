726807544b557416675fc693ca8dd644b04d9063
Andy Zhang <xiazhang@microsoft.com>
2019-05-15 11:44:29 +0800

    Update get-vm-size-list.sh

--- linux/azuredisk/get-vm-size-list.sh
+++ linux/azuredisk/get-vm-size-list.sh
@@ -3,12 +3,12 @@
 az account list-locations -o tsv | awk -F '\t' '{print $5}' > locations
 for location in `cat ./locations`
 do
-	echo "get vm list-sizes on $$location region ..."
+	echo "get vm list-sizes on $location region ..."
 	az vm list-sizes -l $location -o tsv | awk -F '\t' '{print "\""$3"\":"$1","}' | sort | uniq > $location.txt
 done
 
 cat *.txt > vmsizelist.data
-OUTPUT-FILE="vmsizelist.final"
-cat vmsizelist.data | sort | uniq > $OUTPUT-FILE
+OUTPUT="vmsizelist.final"
+cat vmsizelist.data | sort | uniq > $OUTPUT
 
-echo "got all vm list-sizes from all regions, output file: $OUTPUT-FILE"
+echo "got all vm list-sizes from all regions, output file: $OUTPUT"
