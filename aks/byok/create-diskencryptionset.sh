9ea9a9e710f839988ac1566b6658825ea815ac98
Andy Zhang <xiazhang@microsoft.com>
2020-01-03 14:16:48 +0800

    Update create-diskencryptionset.sh

--- aks/byok/create-diskencryptionset.sh
+++ aks/byok/create-diskencryptionset.sh
@@ -7,8 +7,8 @@ keyVaultName=andy-des
 keyName=andy-des
 diskEncryptionSetName=andy-des
 
+# az account set --subscription $subscriptionId
 az group create -g $rgName -l $location
-az account set --subscription $subscriptionId
 az keyvault create -n $keyVaultName -g $rgName -l $location --enable-purge-protection true --enable-soft-delete true
 az keyvault key create --vault-name $keyVaultName -n $keyName --protection software
 keyVaultId=$(az keyvault show --name $keyVaultName --query [id] -o tsv)
