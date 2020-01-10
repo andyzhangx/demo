1f124b2f05d71b2aaaa64cff93dfa99012a8892c
Andy Zhang <xiazhang@microsoft.com>
2020-01-10 15:46:07 +0800

    Update create-diskencryptionset.sh

--- aks/byok/create-diskencryptionset.sh
+++ aks/byok/create-diskencryptionset.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-subscriptionId=
-rgName=andy-diskencryptionset 
+rgName=
 location=southcentralus 
-keyVaultName=andy-des
-keyName=andy-des
-diskEncryptionSetName=andy-des
+keyVaultName=
+keyName=
+diskEncryptionSetName=
 
-# az account set --subscription $subscriptionId
 az group create -g $rgName -l $location
 az keyvault create -n $keyVaultName -g $rgName -l $location --enable-purge-protection true --enable-soft-delete true
 az keyvault key create --vault-name $keyVaultName -n $keyName --protection software
 keyVaultId=$(az keyvault show --name $keyVaultName --query [id] -o tsv)
 keyVaultKeyUrl=$(az keyvault key show --vault-name $keyVaultName --name $keyName --query key.kid -o tsv)
-az group deployment create -g $rgName --template-uri "https://raw.githubusercontent.com/ramankumarlive/manageddiskscmkpreview/master/CreateDiskEncryptionSet.json" --parameters "diskEncryptionSetName=$diskEncryptionSetName" "keyVaultId=$keyVaultId" "keyVaultKeyUrl=$keyVaultKeyUrl" "region=$location"
+az disk-encryption-set create -n $diskEncryptionSetName -g $rgName --key-url $keyVaultKeyUrl --source-vault $keyVaultId -l $location
+# copy down the DiskEncryptionSet id from the output
 desIdentity=$(az ad sp list --display-name $diskEncryptionSetName --query [].objectId -o tsv)
 az keyvault set-policy -n $keyVaultName -g $rgName --object-id $desIdentity --key-permissions wrapkey unwrapkey get
 az role assignment create --assignee $desIdentity --role Reader --scope $keyVaultId
