6643151d17853c85a01deee747c58702097726bc
andyzhangx <xiazhang@microsoft.com>
2021-02-04 07:16:27 +0000

    add comments

--- dev/cifs/install-cifs-module.sh
+++ dev/cifs/install-cifs-module.sh
@@ -2,9 +2,16 @@
 
 set -euo pipefail
 
+echo "downloading cifs-5.4.0-1039.41.patched.tar.gz ..."
 wget -O /tmp/cifs-5.4.0-1039.41.patched.tar.gz https://raw.githubusercontent.com/andyzhangx/demo/master/dev/cifs/cifs-5.4.0-1039.41.patched.tar.gz
 cd /tmp/
 tar -xvf /tmp/cifs-5.4.0-1039.41.patched.tar.gz
 cd cifs-5.4.0-1039.41.patched
+
+echo "build modules ..."
 make -C /usr/src/linux-headers-$(uname -r)/ M=$(pwd) modules
+
+echo "install modules ..."
 modprobe cifs; rmmod cifs; insmod ./cifs.ko
+
+echo "patched cifs driver installed successfully."
